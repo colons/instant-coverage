0c9a5ee354a36f57f6e21f3795f42ac8402e3242
Iain Dawson <iain@bldm.us>
2014-02-28 11:24:05 +0000

    Increment version number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/colons/instant-coverage',
     author='Iain Dawson',
     author_email='pypi@colons.co',
-    version='0.0.8',
+    version='0.0.9',
     license="BSD",
     platforms=['any'],
     packages=['instant_coverage'],
