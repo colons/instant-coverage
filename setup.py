85f21ed86fb255c214eb1f7891c4e5cd6c11423b
Iain Dawson <iain@colons.co>
2015-04-02 14:54:51 +0100

    Be 0.0.14.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/colons/instant-coverage',
     author='Iain Dawson',
     author_email='pypi@colons.co',
-    version='0.0.13',
+    version='0.0.14',
     license="BSD",
     platforms=['any'],
     packages=['instant_coverage'],
