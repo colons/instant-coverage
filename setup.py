d0a8a48f9fc84c89efd135bbca4a0582d88a8c49
colons <code@colons.co>
2017-02-08 14:55:21 +0000

    Be 1.0.
    We finally sorted out #10!

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/colons/instant-coverage',
     author='colons',
     author_email='pypi@colons.co',
-    version='0.0.16',
+    version='1.0.0',
     license='BSD',
     platforms=['any'],
     packages=['instant_coverage'],
