0f24cc2d5e404337efd83f070b5cb1ab6b2d44d3
colons <colons@colons.co>
2020-08-11 11:36:10 +0100

    Specify that we work on Pythons 2.7 and 3.

--- setup.py
+++ setup.py
@@ -39,5 +39,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 )
